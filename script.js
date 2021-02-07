e3364167a7aeb644cb4a97935d59335d4e42be65
Anim Ahmed <animaiub@gmail.com>
2021-02-08 01:41:39 +0200

    updated

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-function displayRecipes() {
-    const searchMeals = document.getElementById('searchInput').value;
-    const foodCart = document.getElementById('foodCart');
-    const link = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeals}`;
-    fetch(link)
-        .then(response => response.json())
-        .then(data => {
-            const recipes = data.meals;
-            if (recipes == null){
-                document.getElementById('error').style.display = 'flex';
-            } else{
-                recipes.forEach(recipe => {
-                    const recipeName = recipe.strMeal;
-                    const recipeImgLink = recipe.strMealThumb;
-                    const newCard = document.createElement('div');
-                    newCard.className = 'recipeCard';
-                    newCard.id = recipe.idMeal;
-                    newCard.innerHTML = `<div class="card rounded m-3" style="width: 18rem;">
-                    <img src=${recipeImgLink} class="card-img-top" alt="Food  image">
-                    <h5 class="card-body card-title text-center"> ${recipeName} </h5>
-                    </div>`
-                    foodCart.appendChild(newCard)
-                });
-            }
-        })
+const searchMeals =() =>{
+    const searchKeyword = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchKeyword}`;
+    //load data
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayFoods(data.meals))
 }
+
+const displayFoods = foods =>{
+    const foodContainer =document.getElementById('food-container');
+    foods.forEach(food =>{
+       const foodDiv =document.createElement('div');
+       foodDiv.className =  'single-result row align-items-center my-3 p-3';
+       foodDiv.innerHTML = `<div class="card rounded m-3" style="width: 18rem;">
+       <img src=${food.strMealThumb} class="card-img-top" alt="Food  image">
+       <h5 class="card-body card-title text-center"> ${food.strMeal} </h5>
+       </div>`
+       foodContainer.appendChild(foodDiv);
+
+    })
+
+
+}
+
+
+
