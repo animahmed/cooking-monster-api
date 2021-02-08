a75a74a8fd9a7379f0555bf6930ed5f2ef821cb4
Anim Ahmed <animaiub@gmail.com>
2021-02-08 18:37:05 +0200

    updated version

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const searchMeals =() =>{
+const searchMeals = async() =>{
     const searchKeyword = document.getElementById('search-field').value;
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchKeyword}`;
     //load data
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayFoods(data.meals))
+    try {
+    const  res = await fetch(url);
+    const data =await res.json();
+    displayFoods(data.meals);
+}
+catch (error){
+    displayError('Sorry!failed to load data')
+}
 }
 
 const displayFoods = foods =>{
     const foodContainer =document.getElementById('food-container');
+    foodContainer.innerHTML = '';
     foods.forEach(food =>{
        const foodDiv =document.createElement('div');
        foodDiv.className =  'single-result row align-items-center my-3 p-3';
@@ -22,11 +28,24 @@ const displayFoods = foods =>{
 
 
 }
-const getFoodList = (list) =>{
-    const url= `https://www.themealdb.com/api/json/v1/1/list.php?c=${list}`;
-    fetch(url)
-    .then(res =>res.json())
-    .then (data =>console.log(data) )
+const getFoodList = async(list) =>{
+    const url= `https://www.themealdb.com/api/json/v1/1/filter.php?i=${list}`;
+    try{
+    const res= await fetch(url);
+    const data = res.json();
+   displayFoodList = (data.meals);
+}
+catch (error){
+    displayError('Sorry!failed to load data')
+}
 }
 
+const displayFoodList = foodlist =>{
+    const foodListDiv = document.getElementById('food-list');
+    foodListDiv.innerText = foodlist;
+}
+const displayError = error =>{
+    const errorTag = document.getElementById('error-message');
+    errorTag.innerText= error;
+}
 
